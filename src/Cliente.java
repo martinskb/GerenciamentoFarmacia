fa79713522fe751a46b2c5153eb7adb722082169
kaio <kaio22barra@gmail.com>
2025-11-25 18:46:23 -0300

    Add Cliente class with properties and methods

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Cliente {
+    private int id;
+    private String nome;
+    private String cpf;
+
+    public Cliente(int id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getCpf() { return cpf; }
+
+    public void setNome(String nome) { this.nome = nome; }
 }
