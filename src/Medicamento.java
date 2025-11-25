b2aaba816ed2a6add6a9d88d131d8dd1a9b8f473
kaio <kaio22barra@gmail.com>
2025-11-25 18:47:42 -0300

    Update Medicamento.java

--- src/Medicamento.java
+++ src/Medicamento.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class Medicamento extends Produto {
+    private boolean exigeReceita;
+    private String fabricante;
 
-public class Medicamento {
+    public Medicamento(int id, String nome, double preco, int estoque,
+                       boolean exigeReceita, String fabricante) {
+        super(id, nome, preco, estoque);
+        this.exigeReceita = exigeReceita;
+        this.fabricante = fabricante;
+    }
+
+    public boolean isExigeReceita() { return exigeReceita; }
+    public String getFabricante() { return fabricante; }
 }
