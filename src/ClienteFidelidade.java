ef78694832dfecd723f0ab490b1faa0695738551
kaio <kaio22barra@gmail.com>
2025-11-25 18:46:34 -0300

    Implement ClienteFidelidade class with discount logic

--- src/ClienteFidelidade.java
+++ src/ClienteFidelidade.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class ClienteFidelidade extends Cliente implements Desconto {
+    private int pontos;
 
-public class ClienteFidelidade {
+    public ClienteFidelidade(int id, String nome, String cpf) {
+        super(id, nome, cpf);
+        this.pontos = 0;
+    }
+
+    public void adicionarPontos(int valor) {
+        pontos += valor;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    @Override
+    public double aplicarDesconto(double valor) {
+        return valor * 0.90; // 10% de desconto
+    }
 }
