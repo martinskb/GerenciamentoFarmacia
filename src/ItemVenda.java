c0bf992298688c12dd54e9e2f8ede9d3dc30e7aa
kaio <kaio22barra@gmail.com>
2025-11-25 18:47:17 -0300

    Update ItemVenda.java

--- src/ItemVenda.java
+++ src/ItemVenda.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class ItemVenda {
+    private Produto produto;
+    private int quantidade;
 
-public class Venda {
+    public ItemVenda(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public double calcularTotal() {
+        return produto.getPreco() * quantidade;
+    }
+
+    public Produto getProduto() { return produto; }
+    public int getQuantidade() { return quantidade; }
 }
