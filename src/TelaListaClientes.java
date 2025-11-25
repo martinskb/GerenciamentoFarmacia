af475e307277927a940c2ddd7d3bc4fc4283cbe3
kaio <kaio22barra@gmail.com>
2025-11-25 18:49:04 -0300

    Extend TelaListaClientes class to JFrame and add table

--- src/TelaListaClientes.java
+++ src/TelaListaClientes.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
-public class TelaListaClientes {
+public class TelaListaClientes extends JFrame {
+
+    public TelaListaClientes(Farmacia farmacia) {
+        setTitle("Lista de Clientes");
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+
+        String[] colunas = {"ID", "Nome", "CPF", "Tipo"};
+
+        DefaultTableModel model = new DefaultTableModel(colunas, 0);
+
+        for (Cliente c : farmacia.getClientes()) {
+            String tipo = (c instanceof ClienteFidelidade) ? "Fidelidade" : "Normal";
+
+            model.addRow(new Object[]{
+                    c.getId(),
+                    c.getNome(),
+                    c.getCpf(),
+                    tipo
+            });
+        }
+
+        JTable tabela = new JTable(model);
+        JScrollPane scroll = new JScrollPane(tabela);
+
+        add(scroll);
+    }
 }
