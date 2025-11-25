98b7bb266620992575994b23047fdec92a32520e
kaio <kaio22barra@gmail.com>
2025-11-25 18:49:22 -0300

    Refactor TelaListaProdutos to extend JFrame

--- src/TelaListaProdutos.java
+++ src/TelaListaProdutos.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
-public class TelaListaProdutos {
+public class TelaListaProdutos extends JFrame {
+
+    public TelaListaProdutos(Farmacia farmacia) {
+        setTitle("Lista de Produtos");
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+
+        String[] colunas = {"ID", "Nome", "Preço", "Estoque"};
+
+        DefaultTableModel model = new DefaultTableModel(colunas, 0);
+
+        for (Produto p : farmacia.getProdutos()) {
+            model.addRow(new Object[]{
+                    p.getId(),
+                    p.getNome(),
+                    p.getPreco(),
+                    p.getEstoque()
+            });
+        }
+
+        JTable tabela = new JTable(model);
+        JScrollPane scroll = new JScrollPane(tabela);
+
+        add(scroll);
+    }
 }
